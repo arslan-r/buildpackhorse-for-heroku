033e866bf32080ceccb05f371a61ca617de38441
Arslan Rakhmankulov <31532955+arslan-r@users.noreply.github.com>
2020-07-22 01:05:38 -0700

    Delete req new relic from client/startup

--- app/imports/startup/client/Startup.jsx
+++ app/imports/startup/client/Startup.jsx
@@ -3,8 +3,6 @@ import { render } from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import App from '../../ui/layouts/App.jsx';
 
-require('newrelic');
-
 
 /** Startup the application by rendering the App layout component. */
 Meteor.startup(() => {
